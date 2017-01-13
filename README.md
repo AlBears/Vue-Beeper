1673e98dab9384e01427e2c270299bee570a4cdd
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-01-13 10:16:11 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# beeper
+
+> Project from https://www.udemy.com/learn-by-doing-vue-js-2-the-right-way
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    {{ name }}
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  data () {
+    return {
+      name: 'ALBEARS'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
