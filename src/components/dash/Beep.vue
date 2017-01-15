f57805cd53104d1ff2bd993c4dbbcde19219fdb7
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-01-15 13:05:43 +0300

    likes

--- src/components/dash/Beep.vue
+++ src/components/dash/Beep.vue
@@ -38,7 +38,16 @@
         props: {beep:{}},
         methods: {
             likeBeep() {
-
+              this.$http.patch(`/beeps/${this.beep.id}/like`)
+                  .then((res) => {
+                    if (this.beep.liked) {
+                      this.beep.likes--;
+                      this.beep.liked = false;
+                    } else {
+                      this.beep.likes++;
+                      this.beep.liked = true;
+                    }
+                  })
             },
 
             beepDate(timestamp) {
